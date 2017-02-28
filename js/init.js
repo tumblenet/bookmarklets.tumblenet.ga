fbbfee2234f17be53f3d6f2b5fafae4f8134070b
Tumble <tumblegamer@gmail.com>
2017-02-28 15:36:05 +0000

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $("#bookmarkme").click(function() {
+    if (window.sidebar) { // Mozilla Firefox Bookmark
+      window.sidebar.addPanel(location.href,document.title,"");
+    } else if(window.external) { // IE Favorite
+      window.external.AddFavorite(location.href,document.title); }
+    else if(window.opera && window.print) { // Opera Hotlist
+      this.title=document.title;
+      return true;
+    }
+  });
+});
