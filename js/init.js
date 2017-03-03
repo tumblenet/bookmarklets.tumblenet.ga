7087295ce488103f83a159fb74c3a11c7791f996
Tumble <tumblegamer@gmail.com>
2017-03-03 11:28:21 +0000

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
-  $("a[id^=jQueryBookmark")
-  $("a#jQueryBookmark").click(function(e){
+  $("a[id^=jQueryBookmark").click(function(e){
     e.preventDefault(); // this will prevent the anchor tag from going the user off to the link
     var bookmarkUrl = this.href;
     var bookmarkTitle = this.title;
