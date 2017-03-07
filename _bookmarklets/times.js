a669c45efad16e8a31af6ffd1d0722197a7b4404
Tumble <tumblegamer@gmail.com>
2017-03-07 11:37:54 +0000

    Create times.js

---
+++ _bookmarklets/times.js
@@ -0,0 +1,9 @@
+# WHEN YOU MAKE YOUR BOOKMARKLEY PUT IT IN THE '_bookmarklets' FOLDER AND MAKE THE FILE EXTENTION '.html'
+---
+layout: bookmarket
+title: Times
+---
+
+Main: function() {
+console.log(this.times);
+},
